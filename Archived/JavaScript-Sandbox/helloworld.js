4a162fd3ac19b8b1856aee4710fc41faa786b245
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-06-27 00:04:00 +0200

    commit

--- Archived/JavaScript-Sandbox/helloworld.js
+++ Archived/JavaScript-Sandbox/helloworld.js
@@ -87,3 +87,10 @@ let myCar = {
     year: 1969
 }
 console.log(myCar);
+
+/////     Arithmetic Operators     /////
+
+console.log(27 + 3.5);
+console.log(2022 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
