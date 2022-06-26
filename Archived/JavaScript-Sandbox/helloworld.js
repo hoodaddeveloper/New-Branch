81985c40a97b96d8462b614b53159116cca25001
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-06-26 21:16:02 +0200

    COMMIT

--- Archived/JavaScript-Sandbox/helloworld.js
+++ Archived/JavaScript-Sandbox/helloworld.js
@@ -4,12 +4,12 @@
 */
 
 // Declaring Variables
-const firstName = 'Hoodad' // String
-let lastName = 'Wesker'
-let nickName = 'TheWesker'
-let age = 27 // Number
+const firstName = 'Hoodad'; // String
+let lastName = 'Wesker';
+let nickName = 'TheWesker';
+let age = 27; // Number
 let isSleeping = false // Boolean
-const skills = ['Valorant', 'Overwatch', 'Developer'] // Array
+const skills = ['Valorant', 'Overwatch', 'Developer']; // Array
 let generalInfo = {
     weight: 55,
     height: 177,
@@ -17,18 +17,18 @@ let generalInfo = {
     isAlive: true
 } // Object
 
-// Reassign the Variable
-nickName = 'TheWesker' // String
+// Reassign the variable
+nickName = 'TheWesker'; // String
 
 // Print Output
-console.log('First Name : ' + firstName) // First Name : Hoodad
-console.log('Last Name : ' + lastName) // Last Name : Wesker
-console.log('NickName : ' + nickName) // NickName : hoodaddeveloper
-console.log('Age : ' + age) // Age : 27
-console.log('Sleeping : ' + isSleeping) // Sleeping : false
-console.log('Skills : ' + skills) // Skills : Valorant, Overwatch, Developer
-console.log('General Info : ' + generalInfo) // General Info : [object Object]
-console.log('Weight : ' + generalInfo.weight) // Weight : 55
+console.log('First Name : ' + firstName); // First Name : Hoodad
+console.log('Last Name : ' + lastName); // Last Name : Wesker
+console.log('NickName : ' + nickName); // NickName : hoodaddeveloper
+console.log('Age : ' + age); // Age : 27
+console.log('Sleeping : ' + isSleeping); // Sleeping : false
+console.log('Skills : ' + skills); // Skills : Valorant, Overwatch, Developer
+console.log('General Info : ' + generalInfo); // General Info : [object Object]
+console.log('Weight : ' + generalInfo.weight); // Weight : 55
 
 // Practicing JavaScript Data Types:
 
