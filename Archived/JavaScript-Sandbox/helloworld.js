8d5c73ed73fe2eabce5922ad452c9ccd2d07ce29
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-06-28 00:14:53 +0200

    new

--- Archived/JavaScript-Sandbox/helloworld.js
+++ Archived/JavaScript-Sandbox/helloworld.js
@@ -4,31 +4,31 @@
 */
 
 // Declaring Variables
-const firstName = 'Hoodad'; // String
-let lastName = 'Wesker';
-let nickName = 'TheWesker';
+const firstName = "Hoodad"; // String
+let lastName = "Wesker";
+let nickName = "TheWesker";
 let age = 27; // Number
-let isSleeping = false // Boolean
-const skills = ['Valorant', 'Overwatch', 'Developer']; // Array
+let isSleeping = false; // Boolean
+const skills = ["Valorant", "Overwatch", "Developer"]; // Array
 let generalInfo = {
-    weight: 55,
-    height: 177,
-    hairColor: 'Black',
-    isAlive: true
-} // Object
+  weight: 55,
+  height: 177,
+  hairColor: "Black",
+  isAlive: true,
+}; // Object
 
 // Reassign the variable
-nickName = 'TheWesker'; // String
+nickName = "TheWesker"; // String
 
 // Print Output
-console.log('First Name : ' + firstName); // First Name : Hoodad
-console.log('Last Name : ' + lastName); // Last Name : Wesker
-console.log('NickName : ' + nickName); // NickName : hoodaddeveloper
-console.log('Age : ' + age); // Age : 27
-console.log('Sleeping : ' + isSleeping); // Sleeping : false
-console.log('Skills : ' + skills); // Skills : Valorant, Overwatch, Developer
-console.log('General Info : ' + generalInfo); // General Info : [object Object]
-console.log('Weight : ' + generalInfo.weight); // Weight : 55
+console.log("First Name : " + firstName); // First Name : Hoodad
+console.log("Last Name : " + lastName); // Last Name : Wesker
+console.log("NickName : " + nickName); // NickName : hoodaddeveloper
+console.log("Age : " + age); // Age : 27
+console.log("Sleeping : " + isSleeping); // Sleeping : false
+console.log("Skills : " + skills); // Skills : Valorant, Overwatch, Developer
+console.log("General Info : " + generalInfo); // General Info : [object Object]
+console.log("Weight : " + generalInfo.weight); // Weight : 55
 
 // Practicing JavaScript Data Types:
 
@@ -42,7 +42,7 @@ console.log(string1 + string2 + string3);
 /////     Number     /////
 
 let myAge = 27;
-let myMoney = 205.50;
+let myMoney = 205.5;
 console.log(myAge + myMoney);
 
 /////     boolean     /////
@@ -50,15 +50,15 @@ console.log(myAge + myMoney);
 let data = true;
 /* let data = false;*/
 if (data) {
-    console.log('Data is true');
+  console.log("Data is true");
 } else {
-    console.log('Data is false');
+  console.log("Data is false");
 }
 
 /////     Null     /////
 
 let n = null;
-    console.log(n * 10);
+console.log(n * 10);
 /* The Answer is "0", because the value of "n" is "null". */
 
 /////     Undefined     /////
@@ -69,8 +69,8 @@ console.log(a * 10);
 
 /////     Symbol     /////
 
-let b = Symbol('b');
-let c = Symbol('c');
+let b = Symbol("b");
+let c = Symbol("c");
 /* console.log(b != c); */
 console.log(b === c);
 
@@ -82,10 +82,10 @@ console.log(x);
 /////     Object     /////
 
 let myCar = {
-    make: 'Ford',
-    model: 'Mustang',
-    year: 1969
-}
+  make: "Ford",
+  model: "Mustang",
+  year: 1969,
+};
 console.log(myCar);
 
 /////     Arithmetic Operators     /////
@@ -96,10 +96,10 @@ console.log(65 / 240);
 console.log(0.2708 * 100);
 
 // Use .toUpperCase() to log 'Codecademy' in all uppercase letters.
-console.log('Codecademy'.toUpperCase());
+console.log("Codecademy".toUpperCase());
 
 // Use a string method to log the following string without whitespace at the beginning and end of it.
-console.log('    Remove whitespace   '.trim());
+console.log("    Remove whitespace   ".trim());
 
 // Inside of a console.log(), create a random number with Math.random(), then multiply it by 100.
 console.log(Math.random() * 100);
@@ -115,19 +115,42 @@ console.log(Number.isInteger(2017)); // true
 console.log(Number.isInteger(2017.5)); // false
 
 /////     Variables     /////
-var favoriteFood = 'pizza';
+
+var favoriteFood = "pizza";
 var numOfSlices = 8;
 console.log(favoriteFood);
 console.log(numOfSlices);
 
 /////     let Variable     /////
+
 let changeMe = true;
 changeMe = false;
 console.log(changeMe);
 
 /////     const Variable     /////
-const entree = 'Enchiladas';
+
+const entree = "Enchiladas";
 console.log(entree);
-entree = 'Tacos' // This will give an type error
+// entree = 'Tacos'; // This will give an type error
 
 /////     Mathematical Assignment Operators     /////
+
+let w = 4;
+w = w + 1;
+console.log(w); // 5
+
+let q = 5;
+q += 1; // Can be written as q = q + 1
+console.log(q); // 6
+
+let s = 6;
+s -= 2; // Can be written as s = s - 2
+console.log(s); // 4
+
+let f = 10;
+f /= 5; // Can be written as f = f / 2
+console.log(f); // 2
+
+let g = 20;
+g *= 2; // Can be written as g = g * 2
+console.log(g); // 10
