eec5ee62fb52ea4a744b601696d4a43536be2eab
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-06-29 01:26:21 +0200

    commit

--- Archived/JavaScript-Sandbox/tutorial-2.js
+++ Archived/JavaScript-Sandbox/tutorial-2.js
@@ -12,3 +12,25 @@ newton = Math.floor(newton);
 console.log(`The temperature is ${newton} degrees Newton.`);
 
 /////     Dog Years Project     /////
+
+// My Age
+const myAge = 27;
+
+// Early Years
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+// Subtracting "myAge"
+let laterYears = myAge - 4;
+
+// Multiplying "laterYears"
+laterYears *= 4;
+
+// My Age in Dog Years
+let myAgeInDogYears = myAge + laterYears;
+
+// Lowercase letters of my name
+let myName = "HOODAD".toLowerCase();
+
+// Display my Age in Dog Years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
