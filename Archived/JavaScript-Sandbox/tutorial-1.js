33aa8a6d3c8c7aaead0f77214d0ff32c0b018c80
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-06-28 16:24:22 +0200

    codding...

--- Archived/JavaScript-Sandbox/tutorial-1.js
+++ Archived/JavaScript-Sandbox/tutorial-1.js
@@ -4,7 +4,16 @@
   First File was the helloworld.js
 */
 
-//     The Increment and Decrement Operator     //
+/////     Literals in JavaScript     /////
+
+// Object {}
+// Boolean true, false
+// String "", ''
+// Template ``
+//
+//
+
+/////     The Increment and Decrement Operator     /////
 
 let a = 10;
 a++;
@@ -15,4 +24,34 @@ b--;
 console.log(b); // 9
 
 /////     String Concatenation with Variables     /////
+
+let myPet = "Short hair Cat";
+console.log("I own a Pet and she is a " + myPet + ".");
+
+/////     String Interpolation     /////
+
+// First Message
+const myMessage1 = `This is my first Message writen in a single line and will be shown in the output just like this.`;
+console.log(myMessage1);
+
+// Second Message
+const myMessage2 = `This is my second Message.
+
+This will be in some different lines with spaces & will be shown
+just like I wrote it like this.
+
+This is because of using Literals in JavaScript ES6.`;
+
+console.log(myMessage2);
+
+// Using Placeholder
+let myName = "Hoodad";
+let myCity = "Shiraz";
+let food = "Kebab";
+
+console.log(`My name is ${myName},
+I live in ${myCity},
+and my favrite food is ${food}.`);
+
+/////     typeof operator     /////
 
