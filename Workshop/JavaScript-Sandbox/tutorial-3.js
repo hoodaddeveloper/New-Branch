e2debc3ff3515e18d547b65408ee091e18221e98
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-06-30 15:07:00 +0200

    commit

--- Workshop/JavaScript-Sandbox/tutorial-3.js
+++ Workshop/JavaScript-Sandbox/tutorial-3.js
@@ -1,2 +1,12 @@
 /////     Codecademy     /////
 
+// An statement that checks if mood is 'sleepy' and tirednessLevel is greater than 8
+
+let mood = 'sleepy';
+let tirednessLevel = 8;
+
+if (mood === 'sleepy' && tirednessLevel > 8) {
+  console.log('time to sleep');
+} else {
+  console.log('not bed time yet');
+}
