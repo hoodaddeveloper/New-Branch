c6033239550ab8173f815bd6dcec121cab3b0860
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-02 00:27:16 +0200

    JS

--- Workshop/JavaScript-Sandbox/tutorial-4.js
+++ Workshop/JavaScript-Sandbox/tutorial-4.js
@@ -108,3 +108,54 @@ function sequentialSizes(val) {
 }
 
 sequentialSizes(1); // The answer should be "Low"
+
+/////     Some mini Projects from Codecademy JavaScript     /////
+
+// You will see random results from the following code
+
+let userName = "Alex";
+if (userName) {
+  console.log(`Hello, ${userName}!`);
+} else {
+  console.log("Hello");
+}
+
+let randomNumber = (Math.floor(Math.random() * 8));
+
+switch (randomNumber) {
+  case 0:
+  console.log('It is certain');
+  break;
+
+  case 1:
+  console.log('It is decidedly so');
+  break;
+
+  case 2:
+  console.log('Reply hazy try again');
+  break;
+
+  case 3:
+  console.log('Cannot predict now');
+  break;
+
+  case 4:
+  console.log('Do not count on it');
+  break;
+
+  case 5:
+  console.log('My sources say no');
+  break;
+
+  case 6:
+  console.log('Outlook not so good');
+  break;
+
+  case 7:
+  console.log('Signs point to yes');
+  break;
+
+  default:
+  console.log('Signs point to yes');
+  break;
+}
