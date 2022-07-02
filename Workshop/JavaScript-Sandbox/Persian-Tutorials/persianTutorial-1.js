1a89f6b9a26a97bd795a29556a67c2d8c7842b08
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-02 14:33:58 +0200

    js

--- Workshop/JavaScript-Sandbox/Persian-Tutorials/persianTutorial-1.js
+++ Workshop/JavaScript-Sandbox/Persian-Tutorials/persianTutorial-1.js
@@ -1,13 +1,16 @@
 // var, let, const
 
 var fname = "Hasan"; // var is a global variable
-var fname = "Ali"; // change the value of fname to "Ali"
-fname = "Samad"; // change the value of fname to "Samad"
+var fname = "Ali"; // changed the value of var to "Ali"
+fname = "Samad"; // changed the value of fname to "Samad"
 
 let age = "40"; // let is a local variable
-//let age = "40"; // error: can't change the value of let
+// let age = "40"; // error: can't change the value of let
+age = "50"; // changed the value of age to "50"
 
 const isMarried = true; // const is a constant variable
+// const isMarried = true; // error: can't change the value of const
+// isMarried = false; // error: can't change the value of isMarried
 
 console.log(fname);
 console.log(age);
