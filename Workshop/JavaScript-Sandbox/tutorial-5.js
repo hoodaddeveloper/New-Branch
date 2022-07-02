b63c52d875207e13b12e31fd616ca4506d25b600
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-02 12:30:16 +0200

    js

--- Workshop/JavaScript-Sandbox/tutorial-5.js
+++ Workshop/JavaScript-Sandbox/tutorial-5.js
@@ -1,4 +1,6 @@
 /////     New project from Codecademy     /////
 
 let raceNumber = Math.floor(Math.random() * 1000); // Generate a random number between 0 and 1000
-console.log(Math.floor(Math.random() * 1000));
+console.log(Math.floor(Math.random() * 1000)); // Print the random number to the console
+
+/////     Now the whole project from Codecademy     /////
