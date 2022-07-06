f0379a47f88e31eeec941e86e2fb7b4b6cfa5ae0
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-06 15:27:15 +0200

    js

---
+++ Workshop/JavaScript-Sandbox/Persian-Tutorials/persianTutorial-2.js
@@ -0,0 +1,15 @@
+///// More about strings /////
+
+let result1;
+let result2;
+let result3;
+let firstName = "John";
+let lastName = "Doe";
+
+result1 = "It's a great course";
+result2 = 'It\'s a great course';
+result3 = firstName + " " + lastName;
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
