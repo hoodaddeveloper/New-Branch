883ad7ee5d4e59375d89c854b03b8b925b17a05c
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-06 15:30:37 +0200

    js

--- Workshop/JavaScript-Sandbox/Persian-Tutorials/persianTutorial-2.js
+++ Workshop/JavaScript-Sandbox/Persian-Tutorials/persianTutorial-2.js
@@ -8,7 +8,7 @@ let lastName = "Doe";
 
 result1 = "It's a great course";
 result2 = 'It\'s a great course';
-result3 = firstName + " " + lastName;
+result3 = `Hello, my first name is ${firstName} and my last name is ${lastName}.`;
 
 console.log(result1);
 console.log(result2);
