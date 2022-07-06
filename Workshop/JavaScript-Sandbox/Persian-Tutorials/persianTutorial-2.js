cdf6b08c726b07a9ce55ae2573506e5025e8445a
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-06 16:43:11 +0200

    js

--- Workshop/JavaScript-Sandbox/Persian-Tutorials/persianTutorial-2.js
+++ Workshop/JavaScript-Sandbox/Persian-Tutorials/persianTutorial-2.js
@@ -3,6 +3,8 @@
 let result1;
 let result2;
 let result3;
+let result4;
+let result5;
 let firstName = "John";
 let lastName = "Doe";
 
@@ -10,6 +12,12 @@ result1 = "It's a great course";
 result2 = 'It\'s a great course';
 result3 = `Hello, my first name is ${firstName} and my last name is ${lastName}.`;
 
+result4 = firstName.toLowerCase(); // "john"
+result5 = lastName.toUpperCase(); // "DOE"
+
 console.log(result1);
 console.log(result2);
 console.log(result3);
+console.log(result4);
+console.log(result5);
+
