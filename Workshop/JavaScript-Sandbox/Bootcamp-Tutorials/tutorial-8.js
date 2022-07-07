9d87d997516d37e9a36de138b2dbc798479e76cf
hoodaddeveloper <mohammadkhaffas@gmail.com>
2022-07-07 16:32:16 +0200

    js

--- Workshop/JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-8.js
+++ Workshop/JavaScript-Sandbox/Bootcamp-Tutorials/tutorial-8.js
@@ -11,5 +11,23 @@ const plantNeedsWater = function (day) {
 plantNeedsWater();
 console.log(plantNeedsWater('Wednesday')); // "true"
 
-///// Arrow Functions /////
+///// ES6 Arrow Functions /////
 
+const newFunction = () => {
+  return 'Hello ES6 World';
+}
+
+console.log(newFunction()); // "Hello ES6 World"
+
+// Concise Body Arrow Functions
+
+const arrowFunction1 = () => {}; // Zero Parameter
+const arrowFunction2 = parameter => {}; // One Parameter
+const arrowFunction3 = (parameter1, parameter2, parameter3) => {}; // Two or more Parameters
+const arrowFunction4 = number => number + number; // Single line function does not need curly braces
+
+// Example:
+const arrowFunction5 = day => day === 'Wednesday' ? true : false;
+
+console.log(arrowFunction5('Wednesday')); // "true"
+console.log(arrowFunction5('sunday')); // "false"
